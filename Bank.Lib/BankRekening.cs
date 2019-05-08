113f1caa99d2b9aaac19443d1a59b6f77aeeb791
Dries Deboosere <dries.deboosere@howest.be>
2019-05-08 17:32:07 +0200

    Add project files.

---
+++ Bank.Lib/BankRekening.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Bank.Lib
+{
+    public class BankRekening
+    {
+        public const string MoneyAmountExceedsBalanceMessage = "Er staat niet genoeg geld op je rekening";
+        public const string MoneyAmountLessThanZeroMessage = "Het bedrag kan niet kleiner dan 0 zijn.";
+        public const string MoneyAmountIsZeroMessage = "Het bedrag kan niet 0 zijn.";
+        public const string MoneyAmountGreaterThanMaxMessage = "Het bedrag kan niet groter zijn dan 2000.";
+
+        public const decimal MaxMoney = 2000M;
+
+        public decimal Balance { get; private set; }
+
+        public BankRekening()
+        {
+            Balance = 500;
+        }
+
+        public BankRekening(decimal beginningBalance)
+        {
+            Balance = beginningBalance;
+        }
+
+        public decimal AddMoneyToBankAccount(decimal money)
+        {
+            Balance += money;
+            return Balance;
+        }
+
+        public decimal TakeMoneyFromBankAccount(decimal money)
+        {
+            if (money < 0)
+            {
+                throw new ArgumentOutOfRangeException(MoneyAmountLessThanZeroMessage);
+            }
+
+            if (Balance < money)
+            {
+                throw new ArgumentOutOfRangeException(MoneyAmountExceedsBalanceMessage);
+            }
+
+            Balance -= money;
+            return Balance;
+        }
+    }
+}
