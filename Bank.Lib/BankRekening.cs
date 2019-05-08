189e54c10c6bf8e3ef92b9a8b13297a5b680d27e
Dries Deboosere <dries.deboosere@howest.be>
2019-05-08 17:32:07 +0200

    Add project files.

---
+++ Bank.Lib/BankRekening.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Bank.Lib
+{
+    public class BankRekening
+    {
+        public decimal Balance { get; private set; }
+
+        public BankRekening()
+        {
+            Balance = 500;
+        }
+
+        public BankRekening(decimal beginningBalance)
+        {
+            Balance = beginningBalance;
+        }
+
+        public decimal AddMoneyToBankAccount(decimal money)
+        {
+            Balance += money;
+            return Balance;
+        }
+
+        public decimal TakeMoneyFromBankAccount(decimal money)
+        {
+            Balance -= money;
+            return Balance;
+        }
+    }
+}
---
+++ BankAutomaat.WPF/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Bank.Lib;
+using System;
+using System.Windows;
+
+namespace BankAutomaat.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BankRekening bankRekening = new BankRekening();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region METHODS
+
+        private void UpdateCurrentAmountLabel()
+        {
+            lblTotalAmount.Content = $"€ {bankRekening.Balance}";
+        }
+
+        private void ClearInputMoneyAmount()
+        {
+            txtMoney.Clear();
+            txtMoney.Focus();
+        }
+
+        private decimal GetMoneyAmountFromUser()
+        {
+            return decimal.Parse(txtMoney.Text);
+        }
+
+        #endregion
+
+        #region EVENTS
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateCurrentAmountLabel();
+            txtMoney.Focus();
+        }
+
+        private void BtnAddMoney_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bankRekening.AddMoneyToBankAccount(GetMoneyAmountFromUser());
+                UpdateCurrentAmountLabel();
+                ClearInputMoneyAmount();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fout: {ex.Message}", "Fout!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BtnGetMoney_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bankRekening.TakeMoneyFromBankAccount(GetMoneyAmountFromUser());
+                UpdateCurrentAmountLabel();
+                ClearInputMoneyAmount();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fout: {ex.Message}", "Fout!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        #endregion
+    }
+}
